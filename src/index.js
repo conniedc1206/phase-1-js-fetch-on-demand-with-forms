4522d5d00d047a4d815f31fc1e3b8c84b7dc2f26
Connie Park <conniedc1206@gmail.com>
2022-05-26 13:11:57 -0700

    commit tree b4a97d454869585af91128aaf9535f02f6a6f694

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
+//Your primary task will be to get this form working. When a user inputs a valid ID, the movie information should appear on the page.
+    //Add event listeners to capture form data and override a form's default behavior
+    //Fetch data based on what the user types into that form
+    //Display that data on the page
+
+//We want to make sure the JavaScript we write executes when the DOM is fully loaded. Any code related to DOM manipulation should either go in init or in a function called within init.
+
 const init = () => {
-  
+    const inputForm = document.querySelector('form')
+    
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const input = document.querySelector('input#searchByID')
+
+        //console.log(input.value)
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
